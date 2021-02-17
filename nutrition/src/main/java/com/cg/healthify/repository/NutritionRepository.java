c15f7abbe4ff3f80bbd654a8c7a7836cd50849ea
sri hari muppalla <sri hari muppalla@LAPTOP-RUAG71KI>
2021-02-17 16:39:29 +0530

    updated

--- nutrition/src/main/java/com/cg/healthify/repository/NutritionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cg.healthify.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cg.healthify.beans.Nutrition;
-
-public interface NutritionRepository extends JpaRepository<Nutrition,Integer>{
-
-}
